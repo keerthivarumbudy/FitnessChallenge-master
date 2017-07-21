1583aa96c32e1f742c604f2ff514ded71e994b32
keerthi varumbudy <keerthi.vrmd@gmail.com>
2017-07-21 16:05:40 +0530

    Gender

--- app/src/main/java/com/example/adithbharadwaj/fitnesschallenge/Result.java
+++ app/src/main/java/com/example/adithbharadwaj/fitnesschallenge/Result.java
@@ -63,6 +63,8 @@ public class Result extends AppCompatActivity{
     public int mcrunch;
 
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -119,9 +121,211 @@ public class Result extends AppCompatActivity{
         PlankResult = (TextView) findViewById(R.id.plankResult);
 
 
+
+        String Gen=prefs.getString("gender","");
+
+
+        if (Gen=="Female") {try{
+            mpush = Integer.parseInt(pushUps);
+        }catch(NumberFormatException ex){ // handle your exception
+            pushResult.setText("Nothing to display yet");
+        }
+
+
+            if(pushUps == null){
+                pushResult.setText("nothing to display yet");
+            }
+
+            if(mpush <=4){
+                pushResult.setText("You did " + mpush + " pushups. You are very poor. click on goals to improve.");
+            }
+
+            if(mpush <=9 && mpush >= 5){
+                pushResult.setText("you did " + pushUps + " pushups. you are below average. click on goals to improve.");
+            }
+
+            if(mpush <= 19 && mpush >= 10){
+                pushResult.setText("you did " + pushUps + " pushups. you are good. click on goals to improve.");
+            }
+
+            if(mpush >=20){
+                pushResult.setText("you did " + pushUps + " pushups. you are excellent. click on goals to improve.");
+            }
+
+
+
+            //pull ups
+
+            try{
+                mpull = Integer.parseInt(pullUps);
+            }catch(NumberFormatException ex){ // handle your exception
+                pullResult.setText("Nothing to display yet");
+            }
+
+
+
+            if(pushUps.toString().trim() == null || pushUps.toString().trim().equals("") || pushUps.toString().trim().isEmpty()){
+                pushResult.setText("nothing to display yet");
+            }
+
+
+            if(pullUps == null){
+                pullResult.setText("nothing to display yet");
+            }
+
+            if(mpull <= 1){
+                pullResult.setText("You did " + pullUps + " Pull-ups. you are very poor. click on goals to improve.");
+            }
+
+            if(mpull <= 5 && mpull >= 2){
+                pullResult.setText("You did " + pullUps + " Pull-ups. you are average. click on goals to improve.");
+            }
+
+            if(mpull <= 10 && mpull >= 6){
+                pullResult.setText("You did " + pullUps + " Pull-ups. you are good. click on goals to improve.");
+            }
+
+            if(mpull >= 10){
+                pullResult.setText("You did " + pullUps + " Pull-ups. you are excellent. click on goals to improve.  ");
+            }
+
+
+
+            //Crunches
+
+            try{
+                mcrunch = Integer.parseInt(crunches);
+            }catch(NumberFormatException ex){ // handle your exception
+                Log.e("", "");
+            }
+
+            if(pullUps.toString().trim() == null || pullUps.toString().trim().equals("") || pullUps.toString().trim().isEmpty()){
+                pullResult.setText("nothing to display yet");
+            }
+
+
+            if(mcrunch == -1){
+                CrunchResult.setText("Nothing to display yet");
+            }
+
+            if(mcrunch <= 18){
+                CrunchResult.setText("You did " + crunches + " crunches. you are very poor. click on goals to improve.");
+            }
+
+            if(mcrunch <= 19 && mcrunch >= 38){
+                CrunchResult.setText("You did " + crunches + " crunches. you are average. click on goals to improve.");
+            }
+
+            if(mcrunch <= 39 && mcrunch >= 49){
+                CrunchResult.setText("You did " + crunches + " crunches. you are good. click on goals to improve.");
+            }
+
+            if(mcrunch >= 50){
+                CrunchResult.setText("You did " + crunches + " crunches. you are excellent. click on goals to improve. but you don't need to, do you? ");
+            }
+
+
+            //Planks
+
+            try{
+                mplank = Integer.parseInt(plank);
+            }catch(NumberFormatException ex){ // handle your exception
+                Log.e("", "");
+            }
+
+            if(crunches.toString().trim() == null || crunches.toString().trim().equals("") || crunches.toString().trim().isEmpty()){
+                CrunchResult.setText("nothing to display yet");
+            }
+
+
+            if(mplank <= 30){
+                PlankResult.setText("You did plank for " + plank + " seconds. you are very poor. click on goals to improve.");
+            }
+
+            if(mplank <= 60 && mplank >= 31){
+                PlankResult.setText("You did plank for " + plank + " seconds. you are average. click on goals to improve.");
+            }
+
+            if(mplank <= 180 && mplank >= 60){
+                PlankResult.setText("You did plank for " + plank + " seconds. you are good. click on goals to improve.");
+            }
+
+            if(mplank >= 181){
+                PlankResult.setText("You did plank for " + plank + " seconds. you are excellent. click on goals to improve. but you don't need to, do you? ");
+            }
+
+            //Running
+
+            try{
+                mrun = Integer.parseInt(running);
+            }catch(NumberFormatException ex){ // handle your exception
+                Log.e("", "");
+            }
+
+            if(plank.toString().trim() == null || plank.toString().trim().equals("") || plank.toString().trim().isEmpty()){
+                PlankResult.setText("nothing to display yet");
+            }
+
+
+            if(mrun >= 300){
+                RunningResult.setText("You ran for " + running + " seconds. you are very poor. click on goals to improve.");
+            }
+
+            if(mrun <= 301  && mrun >= 240 ){
+                RunningResult.setText("You ran for " + running + " seconds. you are average. click on goals to improve.");
+            }
+
+            if(mrun <= 241 && mrun >= 161){
+                RunningResult.setText("You ran for " + running + " seconds. you are good. click on goals to improve.");
+            }
+
+            if(mrun <= 160){
+                RunningResult.setText("You ran for " + running + " seconds. you are excellent. click on goals to improve. but you don't need to, do you? ");
+            }
+
+
+            //Squats
+
+            try{
+                msquat = Integer.parseInt(squats);
+            }catch(NumberFormatException ex){ // handle your exception
+                Log.e("", "");
+            }
+
+            if(running.toString().trim() == null || running.toString().trim().equals("") || running.toString().trim().isEmpty()){
+                RunningResult.setText("nothing to display yet");
+            }
+
+
+            if(msquat <= 20){
+                SquatResult.setText("You did " + squats + " squats. you are very poor. but you can easily improve. click on goals to improve.");
+            }
+
+            if(msquat <= 21  && msquat >= 29 ){
+                SquatResult.setText("You did " + squats + " squats. you are average. click on goals to improve.");
+            }
+
+            if(msquat <= 30 && msquat >= 34){
+                SquatResult.setText("You did " + squats + " squats. you are good. click on goals to improve.");
+            }
+
+            if(msquat >= 35){
+                SquatResult.setText("You did " + squats + " squats. you are excellent. click on goals to improve. but you don't need to, do you? ");
+            }
+
+            else if(msquat == -1){
+                SquatResult.setText("Nothing to display yet");
+            }
+
+
+            if(squats.toString().trim() == null || squats.toString().trim().equals("") || squats.toString().trim().isEmpty()){
+                SquatResult.setText("nothing to display yet");
+            }
+
+      }
       //push up
 
-
+      else
         try{
             mpush = Integer.parseInt(pushUps);
         }catch(NumberFormatException ex){ // handle your exception
