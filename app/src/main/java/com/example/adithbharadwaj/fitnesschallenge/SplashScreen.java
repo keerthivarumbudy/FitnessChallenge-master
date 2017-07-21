d6e2ec0c4162f0dc8bc9ea2a975f7d8ed7e00dda
adith bharadwaj <adithbharadwaj@gmail.com>
2017-07-22 00:31:17 +0530

    fine tuning

--- app/src/main/java/com/example/adithbharadwaj/fitnesschallenge/SplashScreen.java
+++ app/src/main/java/com/example/adithbharadwaj/fitnesschallenge/SplashScreen.java
@@ -38,7 +38,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    sleep(6000);
+                    sleep(5000);
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     startActivity(intent);
                     finish();
